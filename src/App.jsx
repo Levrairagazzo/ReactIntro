8397b79b83cb32358930cd5f93b8ba75acddf1dd
Sydney Goeury <81683710+Levrairagazzo@users.noreply.github.com>
2022-09-30 19:37:13 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import MyComponent from './MyComponent';
+
+export default function App() {
+  return (
+    <main>
+     <MyComponent title="Hello World"/>
+
+    </main>
+  )
+}
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import MyComponent from './MyComponent';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+		      <MyComponent title="Hello World again"/>
+
+	</React.StrictMode>
+)
